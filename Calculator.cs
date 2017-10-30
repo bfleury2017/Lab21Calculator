4920f557653b102387949d7f0837a0909776f763
bfleury2017 <bfleury@outlook.com>
2017-10-29 23:31:02 -0400

    Initial

---
+++ Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lab21Calculator
+{
+    public class Calculator
+    {
+        public static double Number1 { get; set; }
+        public static double Number2 { get; set; }
+
+        public Calculator(double number1, double number2)
+        {
+            Number1 = number1;
+            Number2 = number2;
+        }
+
+        public static double Modulus(double number1, double number2) => number1 % number2;
+
+        public static double Add(double number1, double number2) => number1 + number2;
+
+        public static double Subtract(double number1, double number2) => number1 - number2;
+
+        public static double Divide(double number1, double number2) => number1 * number2;
+
+        public static double Multiply(double number1, double number2) => number1 / number2;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Lab21Calculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                DisplayMenu();
+                int selection = RunSelection();
+
+                //ValidateSelection(ref int selection);
+                if (selection == 6)
+                {
+                    Console.WriteLine("Good bye..");
+                    break;
+                }
+
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                Console.WriteLine();
+
+            } while (true);
+        }
+
+        private static void ValidateSelection()
+        {
+
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("Calculator Menu");
+            Console.WriteLine($"1.  Add");
+            Console.WriteLine($"2.  Subtract");
+            Console.WriteLine($"3.  Multiply");
+            Console.WriteLine($"4.  Divide");
+            Console.WriteLine($"5.  Modulus");
+            Console.WriteLine($"6.  Quit");
+            Console.WriteLine("----------------------------");
+        }
+
+        private static int RunSelection()
+        {
+            Console.Write("Enter your selection [1-6]:");
+            int selection = int.Parse(Console.ReadLine());
+
+            if (selection == 6)
+            {
+                return selection;
+            }
+
+            Console.Write("Enter first number: ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            DoArithmetic(selection, number1, number2);
+            return selection;
+        }
+
+        private static void DoArithmetic(int selection, int number1, int number2)
+        {
+            switch (selection)
+            {
+                case 1:
+                    Console.WriteLine($"{number1} + {number2} = {Calculator.Add(number1, number2)}");
+                    break;
+                case 2:
+                    Console.WriteLine($"{number1} - {number2} = {Calculator.Subtract(number1, number2)}");
+                    break;
+                case 3:
+                    Console.WriteLine($"{number1} * {number2} = {Calculator.Multiply(number1, number2)}");
+                    break;
+                case 4:
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero, please try again with valid values");
+                        break;
+                    }
+                    Console.WriteLine($"{number1} / {number2} = {Calculator.Divide(number1, number2)}");
+
+                    break;
+                case 5:
+                    Console.WriteLine($"{number1} % {number2} = {Calculator.Modulus(number1, number2)}");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
